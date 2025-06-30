e6e3b8b6d1892de0362f40dc5024d311f396c0d8
geovanavenera <geovana.venera@gmail.com>
2025-06-30 10:36:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Botão para mostrar mensagem 
+ <script>
+  function mostrarMensagem() {
+    const msg = document.getElementById("mensagem");
+    msg.textContent = "A esperança é a única coisa mais forte que o medo - Suzanne Collins" 
+        }
+</script> 
+
+<button onclick="mostrarMensagem()">Clique para ver uma mensagem!</button>
+<p id="mensagem"></p>
+
